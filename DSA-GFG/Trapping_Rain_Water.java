8191108cdfb6eea2e3ccebf879a712c34692b558
RyanPaulTirkey <ryanpaultirkey@gmail.com>
2026-05-04 01:42:34 +0530

    changes

--- DSA-GFG/Trapping_Rain_Water.java
+++ DSA-GFG/Trapping_Rain_Water.java
@@ -1,7 +1,42 @@
+import java.util.Scanner;
+
 public class Trapping_Rain_Water {
     public static void main(String[]ar){
          int arr[]={3,0,1,0,4,0,2};
-       int res =0;
+        System.out.println("Choose method 1. brute force 2.more optimised");
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        switch(a){
+            case 1:
+                brute_force(arr);
+                break;
+            case 2:
+                opti(arr);
+                break;
+        }
+    }
+    public static void opti(int arr[]){
+        int res=0;
+        int left =0, right =arr.length-1;
+        int lMax = arr[0], rMax= arr[arr.length-1];
+
+        while (left<right){
+            if(lMax<=rMax){
+                left++;
+                lMax= Math.max(lMax,arr[left]);
+                res+=lMax-arr[left];
+            }
+            else{
+                right--;
+                rMax = Math.max(rMax,arr[right]);
+                res+=rMax-arr[right];
+            }
+            
+        }
+        System.out.println(res);
+    }
+    public static void brute_force(int[]arr){
+        int res =0;
        
        for(int i=1;i<arr.length-1;i++){
           int left = arr[i];
