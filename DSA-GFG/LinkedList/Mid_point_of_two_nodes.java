695c2eb40123efa5ffb0db97ffbb4da8b8fb9337
Ryan <74509851+Bruh-Ryan@users.noreply.github.com>
2026-07-24 17:48:48 +0530

    Add Mid_point_of_two_nodes class for linked list

---
+++ DSA-GFG/LinkedList/Mid_point_of_two_nodes.java
@@ -0,0 +1,92 @@
+class Mid_point_of_two_nodes{
+    public static void main(String[] args) {
+        
+        LinkedList l1 = new LinkedList();
+        l1.push(1);
+        l1.push(2);
+        l1.push(4);
+        
+        Node op = l1.midNode(l1.head);
+        System.out.println(op.data);
+    }
+}
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+class LinkedList{
+    Node head;
+    
+    public void push(int data){
+        if(head==null){
+            head = new Node(data);
+            return;
+        }
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = new Node(data);
+       
+    }
+    public void del_ele(int d){
+        if(head==null){
+            return;
+        }
+        
+        Node prev = head;
+        Node cur = head.next;
+        Node n = null;
+        
+        while(cur != null){
+           if(head.data==d){
+               head=head.next;
+               return;
+           }
+           
+           n=cur.next;
+           if(cur.data == d){
+            //   System.out.println("found "+ cur.data); //debug
+                if(cur.next==null){
+                    // System.out.println("found that cur.next != 10");//debug
+                    // System.out.println("prev"+prev.data);//debug
+                    prev.next=null;
+                    cur=prev.next;
+                }
+                prev.next=n;
+                cur=prev;
+                
+           }
+            // System.out.println("traversed node :"+cur.data);//debug
+            cur=cur.next;
+            prev=prev.next;
+            if(cur.next==null && cur.data != d){
+                System.out.println("NOT FOUND");
+                return;
+            }
+        }
+    }
+    public void showLinkedList(){
+        Node temp=head;
+        System.out.print("head");
+        while(temp!=null){
+            System.out.print("->"+temp.data);
+            temp=temp.next;
+        }
+        System.out.print("->null\n");
+    }
+    public Node midNode(Node node){
+        Node slow = node;
+        Node fast = node;
+        while(fast!=null && fast.next!=null){
+            fast    = fast.next.next;
+            slow    =   slow.next;
+        }
+        return slow;
+    }
+    
+}
